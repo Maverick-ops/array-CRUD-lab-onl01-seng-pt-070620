9778cc3276fc349acc4a2d8f1cfee474613be1c7
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-13 00:55:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  Guys_at_work << (shovel)
+  Guys_at_work << "Kevin"
 end
 
 def add_element_to_start_of_array(array, element)
